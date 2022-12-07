4a16e9997114eba01933f0ac6a65cc8448c31014
d4daveshep <david@daveshep.net.nz>
2022-12-08 11:28:27 +1300

    Day 7 under construction

--- test_day_7.py
+++ test_day_7.py
@@ -1,4 +1,5 @@
 import pytest
+from anytree import Node
 
 
 @pytest.fixture()
@@ -8,7 +9,69 @@ def data():
 
     return data
 
+
+class Command:
+    cd = False
+    ls = False
+    to_dir = ""
+
+class Output:
+    pass
+
+
+def parse_command(line_items: list) -> Command:
+    command = Command()
+    if line_items[0] == "cd":
+        command.cd = True
+        command.to_dir = line_items[1]
+    elif line_items[0] == "ls":
+        command.ls = True
+    else:
+        assert False, f"unknown command: {line_items[0]}"
+
+    return command
+
+
+def parse_output(line_items: list) -> Output:
+    output = Output()
+    if line_items[0] == "dir":
+        output.dir_name = line_items[1]
+    elif line_items[0].isnumeric():
+        output.file_size = int(line_items[0])
+        output.file_name = line_items[1]
+    else:
+        assert False, f"unknown output: {line_items}"
+
+    return output
+
+
+
+
 def test_dir_tree(data):
+    root = Node("/", parent=None)
+    cur_dir = None
+    command = None
+
+    for line in data:
+        line_items = line.split()
+        if line_items[0] == "$":
+            command = parse_command(line_items[1:])
+            if command.cd:
+                print(f"CD {command.to_dir}")
+                if command.to_dir == "/":
+                    cur_dir = root
+                else:
+                    pass
+            elif command.ls:
+                print("LS")
+                pass
+            else:
+                assert False
+
+        else:
+            output = parse_output(line_items)
+
+
     pass
 
 
