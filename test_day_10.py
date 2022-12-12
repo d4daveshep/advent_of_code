b67ba909352bd84e0e1592dfec1d0ebebab17c2d
d4daveshep <david@daveshep.net.nz>
2022-12-13 07:56:57 +1300

    Day 10 part 2 in test but not working yet

--- test_day_10.py
+++ test_day_10.py
@@ -10,7 +10,6 @@ def data_1():
     return data
 
 
-
 def test_5_cycles():
     data = ["noop\n", "addx 3\n", "addx -5\n"]
     program = Program()
@@ -31,3 +30,32 @@ def test_part_1_data(data_1):
     assert program.signal_strength(180) == 2880
     assert program.signal_strength(220) == 3960
     assert program.sum_signal_strength() == 13140
+
+
+def draw_sprite(crt_row:list, cycle: int, x: int) -> list:
+    if x - 1 <= cycle <= x + 1:
+        crt_row[cycle - 1] = "#"
+    return crt_row
+
+
+def display(crt_row:list)->str:
+    return "".join([str(i) for i in crt_row])
+
+
+def test_draw_sprite():
+    crt_row = ["." for i in range(40)]
+    cycle = 1
+    x = 1
+    crt_row = draw_sprite(crt_row, cycle, x)
+    assert crt_row[0] == "#"
+    print(display(crt_row))
+
+def test_crt_row_1(data_1):
+    crt_row = ["." for i in range(40)]
+
+    program = Program()
+    program.process_instructions(data_1)
+    for cycle in range(1,41):
+        x = program.history[cycle]
+        draw_sprite(crt_row, cycle, x)
+    print(display(crt_row))
