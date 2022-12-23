e95895163282d66379ba2b7fd0ae20ef5ddc4144
d4daveshep <david@daveshep.net.nz>
2022-12-23 13:45:18 +1300

    Day 15 part 1 completed

--- day_15.py
+++ day_15.py
@@ -1,3 +1,7 @@
+import time
+from collections import Counter
+
+from aocd.models import Puzzle
 from typing import NamedTuple
 
 import parse
@@ -13,7 +17,22 @@ class InputLine(NamedTuple):
     beacon: Coord
 
 
-def parse_input_line(line: str) -> NamedTuple:
+class Beacons:
+    b_dict = {}
+
+    def add_beacon(self, b: Coord):
+        if b.y not in self.b_dict:
+            self.b_dict[b.y] = set()
+        self.b_dict[b.y].add(b.x)
+
+    def get_beacon_x_set(self, y: int) -> set:
+        if y not in self.b_dict:
+            return set()
+        else:
+            return self.b_dict[y]
+
+
+def parse_input_line(line: str) -> InputLine:
     result = parse.search("Sensor at x={x_s:d}, y={y_s:d}: closest beacon is at x={x_b:d}, y={y_b:d}", line)
     sensor = Coord(result.named["x_s"], result.named["y_s"])
     beacon = Coord(result.named["x_b"], result.named["y_b"])
@@ -21,7 +40,7 @@ def parse_input_line(line: str) -> NamedTuple:
 
 
 class BeaconExclusionZone:
-    def __init__(self, sensor: NamedTuple, beacon: NamedTuple):
+    def __init__(self, sensor: Coord, beacon: Coord):
         self.sensor = sensor
         self.beacon = beacon
         self.rl_distance = rl_dist(self.sensor, self.beacon)
@@ -33,8 +52,59 @@ class BeaconExclusionZone:
 
         return (self.rl_distance * 2) + 1 - (2 * y_diff)
 
+    def x_set(self, y: int) -> set:
+        y_diff = abs(self.sensor.y - y)
+        if y_diff > self.rl_distance:
+            return set()
 
-def rl_dist(sensor: NamedTuple, beacon: NamedTuple) -> int:
+        x_min = self.sensor.x - (self.rl_distance - y_diff)
+        x_max = self.sensor.x + (self.rl_distance - y_diff)
+        x_set = {x for x in range(x_min, x_max + 1)}
+        return x_set
+
+
+def rl_dist(sensor: Coord, beacon: Coord) -> int:
     xs, ys = sensor
     xb, yb = beacon
     return abs(sensor.y - beacon.y) + abs(sensor.x - beacon.x)
+
+
+
+def parse_data(puzzle_input: str) -> list:
+    return puzzle_input.split('\n')
+
+
+def solve_part_1(data: list) -> int:
+    all_x = set()
+    beacons = Beacons()
+    for input_line in data:
+        # print(f"parsing line...{input_line}")
+        line = parse_input_line(input_line.strip())
+        beacons.add_beacon(line.beacon)
+        bxz = BeaconExclusionZone(line.sensor, line.beacon)
+        all_x.update(bxz.x_set(2000000))
+        all_x.difference_update(beacons.get_beacon_x_set(2000000))
+
+    return len(all_x)
+
+
+def solve_part_2(data: list) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    puzzle = Puzzle(year=2022, day=15)
+    puzzle_input = puzzle.input_data
+    data = parse_data(puzzle_input)
+
+    tic = time.perf_counter()
+    part_1_answer = solve_part_1(data)
+    toc = time.perf_counter()
+    print(f"Part 1 answer = {part_1_answer}")
+    print(f"took {(toc - tic) * 1000:0.1f} msec")
+
+    tic = time.perf_counter()
+    part_2_answer = solve_part_2(data)
+    toc = time.perf_counter()
+    print(f"Part 2 answer = {part_2_answer}")
+    print(f"took {(toc - tic) * 1000:0.1f} msec")
--- test_day_15.py
+++ test_day_15.py
@@ -3,7 +3,7 @@ from typing import NamedTuple
 import parse
 import pytest
 
-from day_15 import rl_dist, BeaconExclusionZone, Coord, parse_input_line
+from day_15 import rl_dist, BeaconExclusionZone, Coord, parse_input_line, Beacons, InputLine
 
 
 @pytest.fixture()
@@ -14,7 +14,7 @@ def test_data():
 
 
 @pytest.fixture()
-def line_8_7() -> NamedTuple:
+def line_8_7() -> InputLine:
     return parse_input_line("Sensor at x=8, y=7: closest beacon is at x=2, y=10")
 
 
@@ -32,8 +32,8 @@ def test_build_function():
 
 
 def test_rectilinear_distance():
-    sensor = Coord(x=9, y=16)
-    beacon = Coord(x=10, y=16)
+    sensor = Coord(x=8, y=7)
+    beacon = Coord(x=2, y=10)
     assert rl_dist(sensor, beacon) == 9
 
 
@@ -46,4 +46,21 @@ def test_x_width_at_y(line_8_7):
 
 
 def test_x_set_at_y(line_8_7):
-    pass
+    bxz = BeaconExclusionZone(line_8_7.sensor, line_8_7.beacon)
+    assert bxz.x_set(7) == {x for x in range(-1, 18)}
+    assert bxz.x_set(6) == bxz.x_set(8) == {x for x in range(0, 17)}
+    assert bxz.x_set(-2) == bxz.x_set(16) == {8}
+    assert bxz.x_set(-3) == bxz.x_set(17) == set()
+
+
+def test_all_x_sets(test_data):
+    all_x = set()
+    beacons = Beacons()
+    for input_line in test_data:
+        line = parse_input_line(input_line)
+        beacons.add_beacon(line.beacon)
+        bxz = BeaconExclusionZone(line.sensor, line.beacon)
+        all_x.update(bxz.x_set(10))
+        all_x.difference_update(beacons.get_beacon_x_set(10))
+
+    assert len(all_x) == 26
